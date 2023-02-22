0b816d203d3cec96f5d06ca6328ca3f1162128f2
Nawinkarthik <126088300+Nawinkarthik@users.noreply.github.com>
2023-02-22 20:19:37 +0530

    Add files via upload

---
+++ day1/Mobile.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Mobile {
+
+	private void makeCall() {
+		// TODO Auto-generated method stub
+		System.out.println("Give me a ring ");
+
+	}
+	private void sendMsg() {
+		// TODO Auto-generated method stub
+		System.out.println("Send me Text message");
+
+	}
+	public static void main(String[] args) {
+		Mobile mb=new Mobile();
+		mb.makeCall();
+		mb.sendMsg();
+	}
+	
+}
