97fe93178dcfe2fbb214d2019bd9d662ed66c364
Nawinkarthik <126088300+Nawinkarthik@users.noreply.github.com>
2023-02-22 20:16:57 +0530

    Add files via upload
    Week1-day1-Assignment

---
+++ assignmentsection/Car.java
@@ -0,0 +1,28 @@
+package assignmentsection;
+
+public class Car {
+	private void drivecar() {
+		// TODO Auto-generated method stub
+		System.out.println("Start the engine");
+	}
+	private void applybrake() {
+		// TODO Auto-generated method stub
+System.out.println("Stop the car");
+	}
+private void soundhorn() {
+	// TODO Auto-generated method stub
+System.out.println("Press horn sound");
+}
+private void ispunctur() {
+	// TODO Auto-generated method stub
+	System.out.println("Tyere is flat");
+
+}
+public static void main(String[] args) {
+	Car cj=new Car();
+	cj.drivecar();
+	cj.applybrake();
+	cj.soundhorn();
+	cj.ispunctur();
+}
+}
